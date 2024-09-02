2fba8b389959a73f58851f37d254de7cacca4851
sunnykumar022 <sunnykumar00802@gmail.com>
2024-09-02 20:27:50 +0530

    addition of two float value

---
+++ first.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ int main(){
+//  int num = 10;
+// printf("%d\n", num);
+// int a,b;
+// scanf("%d", &a);
+//  scanf("%d", &b);
+// printf("%d\n", a+b);
+float c,d;
+float f_num = c;
+float f__num = d ;
+
+scanf("%f", & f__num);
+scanf("%f", & f_num);
+printf("%f\n", c+d);
+
+return 0;
+
+
+
+}
